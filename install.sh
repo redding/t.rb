2f653e9a2097418992aaace05b2efb1e204d1e3b
Kelly Redding <kelly@kellyredding.com>
2021-01-06 10:52:38 -0600

    add install/uninstall/release scripts
    These are for managing the tool.

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+set -e
+
+T_HOME_DIR="$HOME/.t.rb"
+T_RELEASE="0.0.1"
+
+# make sure the bin path is in place
+
+      [ -n "$PREFIX" ] || PREFIX="/usr/local"
+      BIN_PATH="$PREFIX/bin"
+      mkdir -p "$BIN_PATH"
+
+# download the release tag and link to the bin path
+
+      mkdir -p "$T_HOME_DIR"
+      pushd "$T_HOME_DIR" > /dev/null &&
+        rm -rf "t.rb-$T_RELEASE"
+        curl -L "https://github.com/redding/t.rb/tarball/$T_RELEASE" | tar xzf - '*/libexec/*'
+        mv *-t.rb-* "t.rb-$T_RELEASE"
+        ln -sf "t.rb-$T_RELEASE/libexec"
+      popd > /dev/null
+
+# install in the bin path
+
+      ln -sf "$T_HOME_DIR/libexec/t.rb" "$BIN_PATH/t"
+
+# done!
+
+      echo "Installed at ${BIN_PATH}/t"
---
+++ release.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+T_RELEASE="0.0.1"
+
+# check uncommitted changes
+
+      if ! git diff-index --quiet HEAD --; then
+        echo "There are files that need to be committed first."
+      else
+        # tag the release
+
+              if git tag -a -m "Release $T_RELEASE" "$T_RELEASE"; then
+                echo "Tagged $T_RELEASE release."
+
+        # push the changes and tags
+
+                if git push && git push --tags; then
+                  echo "Pushed git commits and tags"
+                else
+                  echo "Release aborted."
+                fi
+              else
+                echo "Release aborted."
+              fi
+      fi
